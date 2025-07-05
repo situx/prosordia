abc75a8a32f9282518ef4d6f89a7a96160bb22c9
Timo <situx@users.noreply.github.com>
2025-07-05 19:10:16 +0200

    fixes

--- js/ordia.js
+++ js/ordia.js
@@ -18,7 +18,7 @@ function initThreeJS(domelement,url){
 	height=500
     width=480
 	scene = new THREE.Scene();
-	const gui = new lil.GUI;({autoPlace: false})
+	const gui = new lil.GUI({autoPlace: false})
 	//gui.domElement.id="gui"
     $("#threejsnav").append($(gui.domElement))
 	const geometryFolder = gui.addFolder("Mesh");
@@ -50,9 +50,9 @@ function initThreeJS(domelement,url){
     var light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(20, 20, 0);
     scene.add(light);
-	/*lightingFolder.add(light.position, "x").min(-5).max(5).step(0.01).name("X Position")
+	lightingFolder.add(light.position, "x").min(-5).max(5).step(0.01).name("X Position")
 	lightingFolder.add(light.position, "y").min(-5).max(5).step(0.01).name("Y Position")
-	lightingFolder.add(light.position, "z").min(-5).max(5).step(0.01).name("Z Position")*/
+	lightingFolder.add(light.position, "z").min(-5).max(5).step(0.01).name("Z Position")
 	axesHelper = new THREE.AxesHelper( Math.max(1000, 1000, 1000) );
     scene.add( axesHelper );
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -66,15 +66,15 @@ function initThreeJS(domelement,url){
 	const updateCamera = () => {
 		camera.updateProjectionMatrix();
 	}
-	/*const cameraFolder = geometryFolder.addFolder("Camera");
+	const cameraFolder = geometryFolder.addFolder("Camera");
 	cameraFolder.add(camera, 'fov', 1, 180).name('Zoom').onChange(updateCamera);
     cameraFolder.add(camera.position, 'x').min(-500).max(500).step(5).name("X Position").onChange(updateCamera);
     cameraFolder.add(camera.position, 'y').min(-500).max(500).step(5).name("Y Position").onChange(updateCamera);
     cameraFolder.add(camera.position, 'z').min(-500).max(500).step(5).name("Z Position").onChange(updateCamera);
     gui.add(objects, 'visible').name('Meshes')
-    gui.add(annotations, 'visible').name('Annotations')
+    //gui.add(annotations, 'visible').name('Annotations')
     gui.add(axesHelper, 'visible').name('Axis Helper')
-    gui.add({"FullScreen":toggleFullScreen2}, 'FullScreen')*/
+    gui.add({"FullScreen":toggleFullScreen2}, 'FullScreen')
     document.addEventListener("fullscreenchange",function(){
         if(document.fullscreenElement){
             camera.aspect = width / height;
